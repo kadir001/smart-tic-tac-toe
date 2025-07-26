c38dcf11d447c1be3ca9e2c91f54a710641bbc77
Kadir Akdemir <45338685+kadir001@users.noreply.github.com>
2025-07-26 21:27:30 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  base: "/smart-tic-tac-toe/", // 🔥 important!
   plugins: [react()],
 });
